2e6d9738c6e3591086502ce19ffd1de9988daad5
JasonChen47 <jwc297@cornell.edu>
2023-05-04 17:06:56 -0700

    Initial Commit

---
+++ Jym/JymApp.swift
@@ -0,0 +1,20 @@
+//
+//  JymApp.swift
+//  Jym
+//
+//  Created by John Smith on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct JymApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
